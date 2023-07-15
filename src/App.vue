a7fbdf33b216980082365720d06f48956302b805
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-07-14 23:14:04 -0600

    'finproyectoadmingastoscircledinamico

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, reactive, watch, computed} from 'vue'
+import {ref, reactive, watch, computed, onMounted} from 'vue'
 import Presupuesto from './components/Presupuesto.vue'
 import ControlPresupuestoVue from './components/ControlPresupuesto.vue';
 import Modal from './components/Modal.vue'
@@ -35,6 +35,8 @@ watch(gastos, () => {
   const totalGastado = gastos.value.reduce((total, gasto) => gasto.cantidad + total, 0)
   gastado.value = totalGastado
   disponible.value = presupuesto.value - totalGastado
+
+  localStorage.setItem('gastos', JSON.stringify(gastos.value))
 }, {
   deep:true
 })
@@ -48,12 +50,22 @@ watch(modal, () => {
   deep: true
 })
 
-  // Limpiar formulario
+watch(presupuesto, () => {
+  localStorage.setItem('presupuesto', presupuesto.value)
+})
 
-//reiniciar modal
+onMounted(() => {
+  const presupuestoStorage = localStorage.getItem('presupuesto')
+  if(presupuestoStorage) {
+    presupuesto.value = Number(presupuestoStorage)
+    disponible.value = Number(presupuestoStorage)
+  }
+  const gastosinStorage = localStorage.getItem('gastos')
+  if(gastosinStorage) {
+    gastos.value = JSON.parse(gastosinStorage)
+  }
 
-
-
+})
 
 
 
@@ -128,7 +140,12 @@ const gastosFiltrados = computed(() => {
 })
 
 
-
+const resetApp = () => {
+  if(confirm('Desear reiniciar')) {
+    gastos.value = []
+    presupuesto.value = 0
+  }
+}
 
 
 </script>
@@ -153,6 +170,7 @@ const gastosFiltrados = computed(() => {
         :presupuesto="presupuesto"
         :disponible="disponible"
         :gastado="gastado"
+        @reset-app="resetApp"
        />
 
       </div>
--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,7 +1,10 @@
 <script setup>
-
-import imagen from '../assets/img/grafico.jpg'
+import { computed } from 'vue'
+import CircleProgress from 'vue3-circle-progress'
+import "vue3-circle-progress/dist/circle-progress.css";
 import { formatearCantidad } from '../helpers/index'
+
+defineEmits(['reset-app'])
   
 const props = defineProps({
   presupuesto: {
@@ -19,6 +22,10 @@ const props = defineProps({
   }
 })
 
+const porcentaje = computed(() => {
+  return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
+})
+
 
 
 </script>
@@ -27,11 +34,26 @@ const props = defineProps({
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
-      <img :src="imagen">
+      <p class="porcentajes"> {{ porcentaje }}% </p>
+
+      <CircleProgress
+      :percent="porcentaje"
+      :size="250"
+      :border-width="28"
+      :border-bg-width="28"
+      fill-color="#3b82f6"
+      empty-color="#e1e1e1"
+      />
+      
     
     </div>
     <div class="contenedor-presupuesto">
-      <button class="reset-app">
+      <button
+      type="button"
+      @click="$emit('reset-app')"
+       class="reset-app"
+
+      >
         Reset App
       </button>
 
@@ -59,6 +81,26 @@ const props = defineProps({
 
 
 <style scoped>
+.contenedor-grafico {
+  position: relative;
+
+}
+.porcentajes {
+  position: absolute;
+  margin: auto;
+  top: calc(50% - 1.5rem);
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 100;
+  font-size: 3rem;
+  font-weight: 700;
+  color: rgb(19, 63, 160)
+
+
+
+}
+
 .dos-columnas {
   display: flex;
   flex-direction: column;
